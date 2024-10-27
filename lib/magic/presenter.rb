61430749c5f8e54b66951a02c8e4c0670573d363
Alexander Senko <Alexander.Senko@gmail.com>
2024-10-27 22:59:29 +0700

    Framework: moved `Magic::Decoratable` to concerns

--- lib/magic/presenter.rb
+++ lib/magic/presenter.rb
@@ -6,8 +6,6 @@ require_relative 'presenter/version'
 require_relative 'presenter/engine'
 
 module Magic # :nodoc:
-	autoload :Presentable, 'magic/presentable'
-
 	# Presentation layer for Rails models
 	module Presenter
 		autoload :Base,      'magic/presenter/base'
