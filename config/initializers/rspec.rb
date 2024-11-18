12e812172ab3235037c3f36149b3df5fd7216d26
Alexander Senko <Alexander.Senko@gmail.com>
2024-11-18 17:22:27 +0700

    Integrations / RSpec: added a view context
    * Presenter specs behave like helper ones.
    * Application routing helpers are included.
    * Routing helpers from all the attached engines are included.
    * Presenter and specs share a view context.

--- config/initializers/rspec.rb
+++ config/initializers/rspec.rb
@@ -2,6 +2,17 @@
 
 RSpec.configure do |config|
 	config.include concern(:PresenterExampleGroup) {
-		included { metadata[:type] = :presenter }
+		included do
+			metadata[:type] = :presenter
+
+			include RSpec::Rails::HelperExampleGroup
+
+			Rails.application
+					.then { [ _1, *_1.railties ] }
+					.grep(Rails::Engine)
+					.each { include _1.routes.url_helpers }
+
+			around { Magic::Presenter.with view_context: self, &_1 }
+		end
 	}, file_path: %r'spec/presenters', type: :presenter
 end if defined? RSpec::Core
