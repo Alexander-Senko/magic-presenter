41a54e90341d002c1b9b29c4a166d2ec628fe5ab
Alexander Senko <Alexander.Senko@gmail.com>
2024-12-07 03:58:50 +0700

    Released v1.1.0
    ## Added
    
    - Handling of edge cases in `Magic::Presenter::Base.model_class`:
      - Guess a corresponding model class by name when several matching ones
        are found.
      - Respect overridden `.for`/`.name_for` instead of using the basic
        `Presenter.for` all the time.

--- lib/magic/presenter/base.rb
+++ lib/magic/presenter/base.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'delegate'
-
 module Magic
 	module Presenter
 		# = Magic Presenter
--- lib/magic/presenter/version.rb
+++ lib/magic/presenter/version.rb
@@ -2,6 +2,6 @@
 
 module Magic
 	module Presenter
-		VERSION = '1.1.0.alpha'
+		VERSION = '1.1.0'
 	end
 end
